6d5c625c16211b2cd40a07293a542b467c87ea33
risako070310 <risako070310@gmail.com>
2021-09-12 07:47:57 +0000

    add profile&search page

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'sinatra/reloader' if development?
 require 'sinatra/activerecord'
 require './models'
 require 'dotenv/load'
+require 'net/http'
 
 enable :sessions
 
@@ -38,4 +39,44 @@ end
 get '/logout' do
     session[:user] = nil
     redirect '/'
+end
+
+get '/search' do
+    unless session[:user]
+        redirect '/'
+    end
+    erb :search
+end
+
+post '/search' do
+    uri = URI("https://itunes.apple.com/search")
+    uri.query = URI.encode_www_form({
+        term: params[:keyword],
+        limit: 20
+    })
+    res = Net::HTTP.get_response(uri)
+    json = JSON.parse(res.body)
+    @musics = json["results"]
+    erb :search
+end
+
+post '/post' do
+    Post.create(
+        user_id: session[:user_id],
+        jacket: params[:jacket],
+        artist: params[:artist],
+        album: params[:album],
+        song: params[:song],
+        sample: params[:sample],
+        comment: params[:comment],
+    )
+    redirect '/profile'
+end
+
+get '/profile' do
+    unless session[:user]
+        redirect '/'
+    end
+    @user = User.find(session[:user])
+    erb :home
 end
